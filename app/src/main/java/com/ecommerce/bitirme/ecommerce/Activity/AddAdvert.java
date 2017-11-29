c733f2b146ea3e8d8b66169f4060b316989be068
musa burak boyraz <mbb3458@gmail.com>
2017-11-29 19:37:46 +0300

    kategori sayfaları için düzenleme yapıldı. ilan ver componentleri için id ler yazıldı

--- app/src/main/java/com/ecommerce/bitirme/ecommerce/Activity/AddAdvert.java
+++ app/src/main/java/com/ecommerce/bitirme/ecommerce/Activity/AddAdvert.java
@@ -1,9 +1,8 @@
 package com.ecommerce.bitirme.ecommerce.Activity;
 
 import android.graphics.Paint;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.MenuItem;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -13,8 +12,8 @@ import android.widget.TextView;
 import com.ecommerce.bitirme.ecommerce.R;
 
 public class AddAdvert extends AppCompatActivity {
-
-
+    TextView ilan;
+    ArrayAdapter<String> arrayAdapter;
 
     String[] ilantipi = new String[]{"Satılık", "Kiralık", "Günlük Kiralık", "Devren Satılık"};
 
@@ -26,13 +25,12 @@ public class AddAdvert extends AppCompatActivity {
         Bundle extras = getIntent().getExtras();
         ilanadi.setText(extras.getString("session"));
         ilanadi.setPaintFlags(Paint.UNDERLINE_TEXT_FLAG);
-
-
-        final TextView ilan=(TextView) findViewById(R.id.ilan);
-     final ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this,
-                R.layout.activity_add_advert, ilantipi);
-        arrayAdapter.setDropDownViewResource(R.layout.activity_add_advert);
         Spinner spinner = (Spinner) findViewById(R.id.spinner);
+
+
+        arrayAdapter = new ArrayAdapter<>(this, R.layout.support_simple_spinner_dropdown_item, ilantipi);
+        
+
         spinner.setAdapter(arrayAdapter);
 
         spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
